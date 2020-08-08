20c64539b2c1bd18ceca95ac57f2ba5c2361ba3e
haya14busa <haya14busa@gmail.com>
2020-08-08 07:09:10 +0000

    tweak

--- .github/actions/reviewdog-install/install.sh
+++ .github/actions/reviewdog-install/install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-TEMP="${REVIEWDOG_TEMPDIR}"
-VERSION="${INPUT_REVIEWDOG_VERSION}"
+VERSION="${INPUT_REVIEWDOG_VERSION:-latest}"
 
-echo '🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
+TEMP="${REVIEWDOG_TEMPDIR}"
+if [ -z "${TEMP}" ]; then
+  if [ -n "${RUNNER_TEMP}" ]; then
+    TEMP="${RUNNER_TEMP}"
+  else
+    TEMP="$(mktemp -d)"
+  fi
+fi
 
 INSTALL_SCRIPT='https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh'
 if [ "${VERSION}" == 'nightly' ] ; then
@@ -12,5 +18,9 @@ if [ "${VERSION}" == 'nightly' ] ; then
 fi
 
 mkdir -p "${TEMP}/reviewdog/bin"
+
+echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
+curl -sfL "${INSTALL_SCRIPT}" | sh -s -- -b "${TEMP}/reviewdog/bin" "${VERSION}" 2>&1
+echo '::endgroup::'
+
 echo "::add-path::${TEMP}/reviewdog/bin"
-curl -sfL "${INSTALL_SCRIPT}" | sh -s -- -b "${TEMP}/reviewdog/bin" "${VERSION}"

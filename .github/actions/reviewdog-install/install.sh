818dfb23d6eb90c31531a0cb293ba6c53826afab
haya14busa <haya14busa@gmail.com>
2020-08-15 08:23:14 +0000

    jq inputs

--- .github/actions/reviewdog-install/install.sh
+++ .github/actions/reviewdog-install/install.sh
@@ -4,6 +4,10 @@ echo '=== env ==='
 env
 echo '=== /env ==='
 
+echo '=== jq inputs ==='
+echo "${INPUTS_JSON}" | jq .
+echo '=== /jq inputs ==='
+
 VERSION="${REVIEWDOG_VERSION:-latest}"
 
 TEMP="${REVIEWDOG_TEMPDIR}"
